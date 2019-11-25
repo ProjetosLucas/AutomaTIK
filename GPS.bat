045ef1d71fb33ef2f39d84b92e3d94c6f73fdc57
Your Name <you@example.com>
2019-11-25 00:33:13 -0300

    Add some Scripts

---
+++ GPS.bat
@@ -0,0 +1,2 @@
+powershell.exe -command '"& C:\xampp\htdocs\AutomaTIK\GPS.ps1 "'
+powershell.exe -command "& 'C:\xampp\htdocs\AutomaTIK\GPS.ps1'"
---
+++ GPS.ps1
@@ -0,0 +1,13 @@
+$COM = [System.IO.Ports.SerialPort]::getportnames()
+
+function read-com {
+    $port= new-Object System.IO.Ports.SerialPort COM1,9600,None,8,one
+
+    $port.Open()
+    do {
+        $line = $port.ReadLine()
+        Write-Host $line # Do stuff here
+    }
+    while ($port.IsOpen)
+}
+read-com
