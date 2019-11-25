8b344b7754f28e3cd1416bf0f82b4c823ed1657f
Your Name <you@example.com>
2019-11-25 00:38:42 -0300

    Some differentes

--- GPS.bat
+++ GPS.bat
@@ -1,2 +1,1 @@
-powershell.exe -command '"& C:\xampp\htdocs\AutomaTIK\GPS.ps1 "'
-powershell.exe -command "& 'C:\xampp\htdocs\AutomaTIK\GPS.ps1'"
+powershell -file "C:\xampp\htdocs\AutomaTIK\GPS.ps1"
